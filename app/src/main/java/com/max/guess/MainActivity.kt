274caa6383391dc8dce672fd541e89c355281928
MaxTsai <maxtsai@esobi.com>
2022-03-08 09:28:41 +0800

    Initial commit

---
+++ app/src/main/java/com/max/guess/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.max.guess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() { // : AppCompatActivity() 繼承的意思
+    override fun onCreate(savedInstanceState: Bundle?) { // 這個onCreate方法允許被子類別所複寫的
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        R.drawable.pig
+        R.string.ok
+        R.color.messagecolor
+
+    }
+}
